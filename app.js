40fe78fb4f3b31b87d5a5dde6eccd679e91d364c
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-01-20 12:39:59 +1300

    create event listeners and get form values

--- app.js
+++ app.js
@@ -7,3 +7,17 @@ function Book(title, author, isbn) {
 
 //UI Constructor
 function UI() {}
+
+//Event Listeners
+
+document.getElementById("book-form").addEventListener("submit", function(e) {
+  const title = document.getElementById("title").value,
+    author = document.getElementById("author").value,
+    isbn = document.getElementById("isbn").value;
+
+  const book = new Book(title, author, isbn);
+
+  e.preventDefault();
+});
+
+document.getElementById("book-list");

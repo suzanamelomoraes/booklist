858840c98dcac8d6c0437ea3e3d22cf143f7e816
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-01-20 12:58:19 +1300

    create function to add book that creates row and cols elements and include it to the UI prototype

--- app.js
+++ app.js
@@ -8,6 +8,22 @@ function Book(title, author, isbn) {
 //UI Constructor
 function UI() {}
 
+//Add Book to List
+
+UI.prototype.addBookToList = function(book) {
+  const list = document.getElementById("book-list");
+
+  const row = document.createElement("tr");
+  row.innerHTML = `
+  <td>${book.title}</td>
+  <td>${book.author}</td>
+  <td>${book.isbn}</td>
+  <td><a href="#" class="delete">X<a></td>
+  `;
+
+  list.appendChild(row);
+};
+
 //Event Listeners
 
 document.getElementById("book-form").addEventListener("submit", function(e) {
@@ -18,8 +34,7 @@ document.getElementById("book-form").addEventListener("submit", function(e) {
   const book = new Book(title, author, isbn);
 
   const ui = new UI();
+  ui.addBookToList(book);
 
   e.preventDefault();
 });
-
-document.getElementById("book-list");
